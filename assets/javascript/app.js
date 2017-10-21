069b18c04edce42a9299fd85852fe175349bdb95
Will <wbattles87@yahoo.com>
2017-10-20 19:06:30 -0500

    close

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -2,6 +2,33 @@ $(document).ready(function()
 {
 	var timernumber = 10;
 	var timerIntervaId;
+	var correct = 0;
+	var incorrect = 0;
+
+	$("#qoneathree").attr("value", correct);
+	$("#qtwoathree").attr("value", correct);
+	$("#qthreeaone").attr("value", correct);
+	$("#qfouratwo").attr("value", correct);
+	$("#qfiveathree").attr("value", correct);
+	$("#qsixaone").attr("value", correct);
+
+	$("#qoneaone").attr("value", incorrect);
+	$("#qoneatwo").attr("value", incorrect);
+	$("#qtwoaone").attr("value", incorrect);
+	$("#qtwoatwo").attr("value", incorrect);
+	$("#qthreeatwo").attr("value", incorrect);
+	$("#qthreeathree").attr("value", incorrect);
+	$("#qfouraone").attr("value", incorrect);
+	$("#qfourathree").attr("value", incorrect);
+	$("#qfiveaone").attr("value", incorrect);
+	$("#qfiveatwo").attr("value", incorrect);
+	$("#qsixatwo").attr("value", incorrect);
+	$("#qsixathree").attr("value", incorrect);
+	
+
+
+
+
 
 
 	$(".questionpage").fadeOut();
@@ -14,7 +41,8 @@ $(document).ready(function()
         // fades in the question page on button click
         $(".questionpage").fadeIn();
         timerRun();
-        winLossRecord();
+        correctFunction();
+        incorrectFunction();
     });
 
 
@@ -24,7 +52,8 @@ $(document).ready(function()
         // fades in the question page on button click
         $(".questionpage").fadeOut();
         $(".finalPage").fadeIn();
-        winLossRecord();
+        correctFunction();
+        incorrectFunction();
     });
         
 
@@ -58,11 +87,78 @@ $(document).ready(function()
 		clearInterval(timerIntervaId);
 	}
 
-    function winLossRecord()
-    {
-        $(".finalPage").html("You Right and Wrong Record is: ")
 
+
+
+
+    $("#qoneathree").on("click", function()
+    {
+		var userclick1 = $(this).attr("value");
+		correctFunction(userclick1);
+
+    });
+    
+    $("#qtwoathree").on("click", function()
+    {
+    	var userclick2 = $(this).attr("value");
+    	correctFunction(userclick2);
+
+    });
+
+	$("#qthreeaone").on("click", function()
+	{
+		var userclick3 = $(this).attr("value");
+		correctFunction(userclick3);
+
+	});
+
+	$("#qfouratwo").on("click", function()
+	{
+		var userclick4 = $(this).attr("value");
+		correctFunction(userclick4);
+
+	});
+
+	$("#qfiveathree").on("click", function()
+	{
+		var userclick5 = $(this).attr("value");
+		correctFunction(userclick5);
+
+	});
+
+	$("#qsixaone").on("click", function()
+	{
+		var userclick6 = $(this).attr("value");
+		correctFunction(userclick6);
+
+	});
+
+	$("#qoneaone, #qoneatwo, #qtwoaone, #qtwoatwo, #qthreeatwo, #qthreeathree, #qfouraone, #qfourathree, #qfiveaone, #qfiveatwo, #qsixatwo, #qsixathree").on("click", function()
+	{
+		var userclick = $(this).attr("value");
+		incorrectFunction(userclick);
+
+	});
+
+    function incorrectFunction()
+    {
+		var newNum = parseInt(userClickValue);
+		correct += newNum;
+		$(".finalPage").text(correct);
     }
+
+
+
+    function correctFunction()
+    {
+    	var newNum = parseInt(userClickValue);
+		incorrect += newNum;
+		$(".finalPage").text(incorrect);
+    }
+
+
+
+
 
 
 });
